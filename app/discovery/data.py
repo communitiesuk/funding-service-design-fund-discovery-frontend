c868a4772642da835d10ba389412806a523ef6d0
Ram <rmn24in@gmail.com>
2022-03-08 11:45:54 +0000

    response data validation added

--- app/discovery/data.py
+++ app/discovery/data.py
@@ -10,30 +10,30 @@ def rounds_search(endpoint):
     with given endpoint and run it through the model class 
     & renders to the template 
     """
-    fund_rounds_data = requests.get(endpoint).json()
-    fund_details = []
-    if fund_rounds_data:
-        for f in fund_rounds_data:
-            rounds_data = model.RoundStoreModel(
-                fund_id=f['fund_id'],
-                round_title=f['round_title'],
-                round_id=f['round_id'],
-                eligibility_criteria=f['eligibility_criteria'],
-                opens= f['opens'],
-                deadline= f['deadline']
-                )
-            # -format the datetime string for opens & deadline
-            rounds_data.format_opens(rounds_data.opens)
-            rounds_data.format_deadline(rounds_data.deadline)
-
-
-            fund_details.append(rounds_data)
+    response = requests.get(endpoint)
+    if response.status_code ==200:
+        fund_rounds_data = response.json()
+        fund_details = []
+        if fund_rounds_data:
+            for f in fund_rounds_data:
+                rounds_data = model.RoundStoreModel(
+                    fund_id=f['fund_id'],
+                    round_title=f['round_title'],
+                    round_id=f['round_id'],
+                    eligibility_criteria=f['eligibility_criteria'],
+                    opens= f['opens'],
+                    deadline= f['deadline']
+                    )
+        
+                # -format the datetime string for opens & deadline
+                rounds_data.format_opens(rounds_data.opens)
+                rounds_data.format_deadline(rounds_data.deadline)
+                fund_details.append(rounds_data)
     else:
         message = "No rounds available"
         return render_template("fund.html", 
                         message = message
                         )
-
     return render_template("fund.html", 
                             fund_rounds_data = fund_rounds_data,
                             fund_details = fund_details,
