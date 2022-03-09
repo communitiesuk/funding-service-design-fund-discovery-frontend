c508d087c1d84e76f102e7243d6a81f50b4f5a8f
Ram <rmn24in@gmail.com>
2022-03-09 09:27:38 +0000

    Docstring added

--- app/discovery/forms.py
+++ app/discovery/forms.py
@@ -3,6 +3,9 @@ from wtforms import StringField
 from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
+    """
+    Given class is a form class model for search fund
+    """
     search = StringField('Search fund', validators=[DataRequired()])
 
     
