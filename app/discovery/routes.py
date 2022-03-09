7260d4da8e8f70127da36c4e6f8bacc49b657db2
Ram <rmn24in@gmail.com>
2022-03-09 16:45:16 +0000

    query_fund imported from data.py

--- app/discovery/routes.py
+++ app/discovery/routes.py
@@ -1,15 +1,13 @@
 from flask import Blueprint, render_template
 from app.discovery.forms import SearchForm
-from app.discovery.models import model
+from app.discovery.data import query_fund
 from app.discovery.models._endpoints import FUND_SEARCH_ENDPOINT, ROUND_SEARCH_ENDPOINT
 from app.discovery.data import rounds_search
 
 discovery_bp = Blueprint("discovery_bp", __name__,  template_folder="templates")
 
-
-
 @discovery_bp.route('/', methods=['GET', 'POST'])
-def search_form():
+def search_fund():
     """ 
     Given function creates a text field for search box &
     renders on index.html then 
@@ -17,12 +15,10 @@ def search_form():
     function query_fund from models to grab the fund 
     & renders back onto index.html 
     """
-
-
     form = SearchForm()
     if form.validate_on_submit():
         query = form.search.data.split(" ")
-        fund_results = model.query_fund(query, FUND_SEARCH_ENDPOINT)
+        fund_results = query_fund(query, FUND_SEARCH_ENDPOINT)
         return render_template("search.html", query =query, 
                               fund_results = fund_results,
                               form=form)
@@ -35,11 +31,8 @@ def fund_rounds(id):
     Function calls the function from data model to 
     check rounds with given endpoint & id. 
     """
-
     rounds = rounds_search(f"{ROUND_SEARCH_ENDPOINT}/{id}")
     return rounds
-
-
 
 
 
