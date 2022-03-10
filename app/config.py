2fd388d80cab0115b80f44d212828a156741c0d9
Ram <rmn24in@gmail.com>
2022-03-10 12:33:05 +0000

    Apis config updated

--- app/config.py
+++ app/config.py
@@ -1,6 +1,5 @@
 """Flask configuration."""
 import os
-from os import path
 
 """
 Application Config
@@ -10,22 +9,12 @@ SESSION_COOKIE_NAME = os.environ.get("SESSION_COOKIE_NAME") or "session_cookie"
 STATIC_FOLDER = "static"
 TEMPLATES_FOLDER = "templates"
 
-
 """
 APIs Config: contains api host imported from manifest.yml.
 & string variables set for test_fund & test_round in the event of 
 host api doesn't not work/respond
 """
-TEST_FUND_STORE_API_HOST = "fund_store"
-TEST_ROUNDS_STORE_API_HOST = "rounds_store"
 
-FUND_STORE_API_HOST = (
-    os.environ.get("FUND_STORE_API_HOST") or TEST_FUND_STORE_API_HOST
-)
+FUND_STORE_API_HOST="https://funding-service-design-fund-store-dev.london.cloudapps.digital"
 
-ROUND_STORE_API_HOST = (
-    os.environ.get("ROUND_STORE_API_HOST")
-    or TEST_ROUNDS_STORE_API_HOST
-)
-
-
+ROUND_STORE_API_HOST="https://funding-service-design-round-store-dev.london.cloudapps.digital"
