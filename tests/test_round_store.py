24126f275d4ed90a0fed65fe19b2b9f9a740c500
Ram <rmn24in@gmail.com>
2022-03-10 13:31:41 +0000

    endpoints updated

--- tests/test_round_store.py
+++ tests/test_round_store.py
@@ -1,21 +1,10 @@
 import requests
-from app.discovery.models._endpoints import (ROUND_SEARCH_ENDPOINT,
-                                             TEST_ROUND_SEARCH_ENDPOINT_RESPONSE)
-
-# ERROR MESSAGE: requests.exceptions.TooManyRedirects: Exceeded 30 redirects.
-
-# def test_round_search_endpoint_response():
-#     """ 
-#     GIVEN our round store api.
-#     WHEN a request made to the round-store endpoint to check the status code
-#     with no data in return.
-#     """
-
-#     response = requests.get(TEST_ROUND_SEARCH_ENDPOINT_RESPONSE)
-#     assert response.status_code == 200
+from app.config import  ROUND_STORE_API_HOST, ROUND_ENDPOINT, ROUND_ID
+from app.discovery.data import get_data
 
 
-id = "funding-service-design"
+expected_data = get_data(f"{ROUND_STORE_API_HOST}/{ROUND_ENDPOINT}/{ROUND_ID}")
+
 def test_round_store_data():
     """ 
     GIVEN our store api.
@@ -24,32 +13,7 @@ def test_round_store_data():
     & then
     data checked against expected contents.
     """
-    expected_contents = [
-        {'fund_id': 'funding-service-design',
-        'round_title': 'Spring', 'round_id': 'spring', 
-        'eligibility_criteria': {'max_project_cost': 1200000},
-        'opens': '2022-02-01T00:00:01', 
-        'deadline': '2022-06-01T00:00:00',
-        'application_url': 'https://funding-service-design-form-runner.london.cloudapps.digital/funding-application'
-        },
-        {'fund_id': 'funding-service-design', 
-        'round_title': 'Summer',
-        'round_id': 'summer',
-        'eligibility_criteria': {'max_project_cost': 1500000},
-        'opens': '2022-06-01T00:00:01', 
-        'deadline': '2022-08-31T00:00:00', 
-        'application_url': 'https://funding-service-design-form-runner.london.cloudapps.digital/funding-application'
-        },
-        {'fund_id': 'funding-service-design',
-        'round_title': 'Autumn',
-        'round_id': 'autumn',
-        'eligibility_criteria': {'max_project_cost': 10400000},
-        'opens': '2022-09-01T00:00:01', 
-        'deadline': '2022-11-30T00:00:00', 
-        'application_url': 'https://funding-service-design-form-runner.london.cloudapps.digital/funding-application'
-        }
-        ]
-
-    rounds = requests.get(f"{ROUND_SEARCH_ENDPOINT}/{id}").json()
-    assert rounds == expected_contents
+    rounds = requests.get(f"{ROUND_STORE_API_HOST}/{ROUND_ENDPOINT}/{ROUND_ID}").json()
+    assert rounds == expected_data
     
+
