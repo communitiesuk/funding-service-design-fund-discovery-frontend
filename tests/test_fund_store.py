7b28e3acf7fa5b72ab62acac97fe533a38159284
Ram <rmn24in@gmail.com>
2022-03-10 13:31:17 +0000

    endpoints updated

--- tests/test_fund_store.py
+++ tests/test_fund_store.py
@@ -1,17 +1,20 @@
-import requests
 from app.discovery.data import query_fund
-from app.discovery.models._endpoints import (FUND_SEARCH_ENDPOINT, 
-                                             TEST_FUND_SEARCH_ENDPOINT_RESPONSE)
+from app.config import FUND_STORE_API_HOST, FUND_ENDPOINT
 
-def test_funds_store_endpoint_response():
-    """ 
-    GIVEN our fund store api. 
-    WHEN a request made to the fund-store endpoint to check the status code
-    with no data in return.
-    """
-    response = requests.get(TEST_FUND_SEARCH_ENDPOINT_RESPONSE)
-    assert response.status_code == 200
-
+expected_contents = [
+    {'fund_name': "Harry's breakfast fund", 
+    'fund_id': 'harry-s-breakfast-fund',
+        'fund_description': "A fund designed to supply Harry's endless supply of muesli."
+        }, 
+        {'fund_name': "Ram's Get Fit Feb fund",
+        'fund_id': 'ram-s-get-fit-feb-fund', 
+        'fund_description': 'A fund designed to supply gym memberships to home workers during Feb.'
+        }, 
+        {'fund_name': 'Funding service design', 
+        'fund_id': 'funding-service-design',
+        'fund_description': 'A fund designed to test the funding service design dev team.'
+        }
+        ]
 
 def test_funds_store_data():
     """
@@ -22,22 +25,7 @@ def test_funds_store_data():
     funds checked against expected contents.
     """
 
-    expected_contents = [
-        {'fund_name': "Harry's breakfast fund", 
-        'fund_id': 'harry-s-breakfast-fund',
-         'fund_description': "A fund designed to supply Harry's endless supply of muesli."
-         }, 
-         {'fund_name': "Ram's Get Fit Feb fund",
-          'fund_id': 'ram-s-get-fit-feb-fund', 
-          'fund_description': 'A fund designed to supply gym memberships to home workers during Feb.'
-          }, 
-          {'fund_name': 'Funding service design', 
-          'fund_id': 'funding-service-design',
-           'fund_description': 'A fund designed to test the funding service design dev team.'
-           }
-           ]
-
-    funds = query_fund("fund", FUND_SEARCH_ENDPOINT)
+    funds = query_fund("fund", f"{FUND_STORE_API_HOST}/{FUND_ENDPOINT}/")
     assert funds == expected_contents
     
 
