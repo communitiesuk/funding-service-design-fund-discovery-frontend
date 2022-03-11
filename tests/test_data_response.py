0f7d67d3280bf2d343df37b61b86add8b094cc9a
Ram <rmn24in@gmail.com>
2022-03-11 23:38:37 +0000

    simplified test_data_response file

--- tests/test_data_response.py
+++ tests/test_data_response.py
@@ -1,50 +1,14 @@
-
-import requests 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import pytest
-from selenium.webdriver.common.by import By
-import time
+from tests.data import (
+    SeleniumElements, HOMEPAGE, RESPONSE_PAGE,
+    SEARCH_BOX_ID, SUBMIT_BUTTON_CLASS, SEARCH_DATA_KEYWORD)
 
-
-
-FUNDS_URL = 'http://127.0.0.1:5000/'
-ROUNDS_URL= 'http://127.0.0.1:5000/round/funding-service-design'
-SELECT_ID = 'search'
-SELECT_CLASS = 'govuk-button--secondary'
-DATA = 'fund'
-
-def get_data(funds_url, rounds_url, selector_one,
-             selector_two,data):
-    """
-    Given test function post the data from 
-    discover store to funds store to grab the data 
-    & when/if response & data is received from funds store
-    then grabs the data from rounds store to grab the data &
-    checks the response 
-    """
-    driver.get(funds_url)
-    to_search = driver.find_element(By.ID, selector_one)
-    time.sleep(1)
-    to_search.click()
-    to_search.send_keys(data)
-    submit = driver.find_element(By.CLASS_NAME,selector_two)
-    submit.click()
-    fund_response = requests.get(FUNDS_URL)
-    time.sleep(1)
-    href_links = driver.find_element(
-        By.XPATH, '//*[@id="main-content"]/div[4]/div/h1/a')
-    href_links.click()
-    time.sleep(1)
-    rounds_response = requests.get(rounds_url)
-    assert fund_response.status_code == 200
-    assert rounds_response.status_code == 200
- 
-
-@pytest.fixture()
-def test_setup(request):
+@pytest.fixture(scope="class")
+def selenium_chrome_driver():
     """
     GIVEN function  returns a Selenium Chrome driver 
     as a fixture for testing.
@@ -52,14 +16,13 @@ def test_setup(request):
     global driver
     options = Options()
     options.add_argument("--headless")
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()))
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     yield 
     driver.close()
 
 
-
-def test_post_data_response(test_setup):
+@pytest.mark.usefixtures("selenium_chrome_driver")
+def test_post_data_response():
     """
     Given test function post the data from 
     discover store to funds store to grab the data 
@@ -67,7 +30,8 @@ def test_post_data_response(test_setup):
     then grabs the data from rounds store to grab the data &
     checks the response 
     """
-    get_data(funds_url = FUNDS_URL, 
-    rounds_url= ROUNDS_URL, selector_one=SELECT_ID,
-    selector_two=SELECT_CLASS, data=DATA)
 
+    search_response = SeleniumElements(homepage=HOMEPAGE,
+    response_page=RESPONSE_PAGE, selenium_id=SEARCH_BOX_ID,
+    selenium_class=SUBMIT_BUTTON_CLASS, search_data=SEARCH_DATA_KEYWORD, driver =driver)
+    search_response.get_data(driver)
