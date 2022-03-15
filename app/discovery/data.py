77553e314c80e9477bf619ed59d765dd48592d3b
Ram <rmn24in@gmail.com>
2022-03-15 08:28:14 +0000

    unused function removed

--- app/discovery/data.py
+++ app/discovery/data.py
@@ -1,45 +1,5 @@
 
-from urllib import response
-from app.discovery.models import rounds
 import requests
-import os
-import json
-from flask import render_template
-from app.config import FLASK_ROOT
-
-def rounds_search(endpoint):
-    """ 
-    Given function is called in routes.py.
-    Function requests the rounds data from round-store-datbase
-    with given endpoint and run it through the model class 
-    & renders to the template 
-    """
-    response = requests.get(endpoint)
-    if response.status_code ==200:
-        fund_rounds_data = response.json()
-        fund_details = []
-        if fund_rounds_data:
-            for fund_rounds in fund_rounds_data:
-                rounds_data = rounds.RoundStore(
-                    fund_id=fund_rounds['fund_id'],
-                    round_title=fund_rounds['round_title'],
-                    round_id=fund_rounds['round_id'],
-                    eligibility_criteria=fund_rounds['eligibility_criteria'],
-                    opens= fund_rounds['opens'],
-                    deadline= fund_rounds['deadline'],
-                    application_url=fund_rounds['application_url']
-                    )
-                fund_details.append(rounds_data)
-    else:
-        message = "No rounds exist for this fund"
-        return render_template("fund.html", 
-                        message = message
-                        )
-    return render_template("fund.html",
-                            fund_rounds_data = fund_rounds_data,
-                            fund_details = fund_details,
-                            rounds_data = rounds_data
-                            )
 
 def query_fund(keyword, endpoint):
     """
@@ -52,7 +12,5 @@ def query_fund(keyword, endpoint):
     if response.status_code == 200:
         query_results = response.json()
         return query_results
-
-
 
 
