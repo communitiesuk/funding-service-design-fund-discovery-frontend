268bdf9e0d5300805eb9092ddcd6566efb3a31b4
Ram <rmn24in@gmail.com>
2022-03-15 08:29:28 +0000

    fund_rounds function refactored

--- app/discovery/routes.py
+++ app/discovery/routes.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, render_template
 from app.discovery.forms import SearchForm
 from app.discovery.data import query_fund
-from app.discovery.data import rounds_search
+from app.discovery.models.rounds import RoundStore
+import requests
 from app.config import (
     FUND_STORE_API_HOST, ROUND_STORE_API_HOST, FUND_ENDPOINT, ROUND_ENDPOINT
 )
 
-discovery_bp = Blueprint("discovery_bp", __name__,  template_folder="templates")
+discovery_bp = Blueprint("discovery_bp", __name__,
+  template_folder="templates")
 
+query = []
 @discovery_bp.route('/', methods=['GET', 'POST'])
 def search_fund():
     """ 
@@ -19,21 +22,47 @@ def search_fund():
     """
     form = SearchForm()
     if form.validate_on_submit():
+        global query
         query = form.search.data.split(" ")
-        fund_results = query_fund(query, f"{FUND_STORE_API_HOST}/{FUND_ENDPOINT}")
+        fund_results = query_fund(
+            query, f"{FUND_STORE_API_HOST}/{FUND_ENDPOINT}")
         return render_template("search.html", query =query, 
                               fund_results = fund_results,
                               form=form)
     else:
         render_template("404.html")                          
     return render_template("search.html", form=form)    
- 
+
 
 @discovery_bp.route('/round/<id>', methods=['GET', 'POST'])
 def fund_rounds(id):
-    """
-    Function calls the function from data model to 
-    check rounds with given endpoint & id. 
-    """
-    rounds = rounds_search(f"{ROUND_STORE_API_HOST}/{ROUND_ENDPOINT}/{id}")
-    return rounds
+    fund_results = query_fund(
+        query, f"{FUND_STORE_API_HOST}/{FUND_ENDPOINT}")
+    response  = requests.get(
+        f"{ROUND_STORE_API_HOST}/{ROUND_ENDPOINT}/{id}")
+    print(response.content)
+    if response.status_code == 200:
+        fund_rounds_data = response.json()
+        fund_details = []
+        if fund_rounds_data:
+            for fund_rounds in fund_rounds_data:
+                rounds_data = RoundStore(
+                    fund_id=fund_rounds['fund_id'],
+                    round_title=fund_rounds['round_title'],
+                    round_id=fund_rounds['round_id'],
+                    eligibility_criteria=fund_rounds['eligibility_criteria'],
+                    opens= fund_rounds['opens'],
+                    deadline= fund_rounds['deadline'],
+                    application_url=fund_rounds['application_url']
+                    )
+                fund_details.append(rounds_data)
+    else:
+        message = "No rounds exist for this fund"
+        return render_template("fund.html", 
+                        message = message
+                        )
+    return render_template("fund.html", 
+                            fund_details = fund_details, 
+                            fund_results = fund_results    
+                        )
+
