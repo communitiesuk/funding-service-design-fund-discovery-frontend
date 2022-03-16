413b29fbccb33fc633e4e1fdae58489e883c8310
Ram <rmn24in@gmail.com>
2022-03-16 09:40:31 +0000

    function refactored

--- app/discovery/data.py
+++ app/discovery/data.py
@@ -1,33 +1,27 @@
 import requests
-from app.discovery.models.rounds import RoundStore
+
 
 def query_fund(keyword, endpoint):
     """
     GIVEN function is called to query the funds.
-    WHEN a query searched by the user, this function runs 
+    WHEN a query searched by the user, this function runs
     to retrive the query results from fund store
     """
-    response = requests.post(endpoint, 
-    params={'search_items':",".join(keyword)})
+    response = requests.post(
+        endpoint, params={"search_items": ",".join(keyword)}
+    )
     if response.status_code == 200:
         query_results = response.json()
         return query_results
 
 
-def query_fund_rounds(fund_rounds):
-    """
-    GIVEN function is a data class from app/models/rounds.py
-     & is called in app/discovery/routes fund_rounds
-      to retrive the data from round store
-    """
-    store_rounds_data = RoundStore(
-                    fund_id=fund_rounds['fund_id'],
-                    round_title=fund_rounds['round_title'],
-                    round_id=fund_rounds['round_id'],
-                    eligibility_criteria=fund_rounds['eligibility_criteria'],
-                    opens= fund_rounds['opens'],
-                    deadline= fund_rounds['deadline'],
-                    application_url=fund_rounds['application_url']
-                    )
-    return store_rounds_data
-    
+def request_json_fund(api_endpoints):
+
+    response = requests.get(api_endpoints)
+
+    if response.status_code == 200:
+        return response.json()
+
+
+def json_funds():
+    pass
