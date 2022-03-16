3ae3fef72129efb619957c1f9f3dd3ff90fb2b49
Ram <rmn24in@gmail.com>
2022-03-16 09:41:08 +0000

    function refactored

--- app/discovery/models/rounds.py
+++ app/discovery/models/rounds.py
@@ -1,39 +1,70 @@
-"""
-model.py contains classes & functions to be called
-in for data processing & data alteration
-"""
-from datetime import datetime
 from dataclasses import dataclass
+from datetime import datetime
+
 
 @dataclass
-class RoundStore:
+class Rounds:
+    """_summary_  rounds class for rounds data
+    alteration
+
+    Returns:
+        Altered simplified rounds data
     """
-    GIVEN class is a dataclass to manage Rounds
-    """
+
     fund_id: str
     round_title: str
     round_id: str
-    eligibility_criteria :dict
+    eligibility_criteria: dict
     opens: datetime
-    deadline: datetime    
+    deadline: datetime
     application_url: str
+
+    @staticmethod
+    def fund_rounds(fund_round):
+        """Function is used for data alteration
+        from class Rounds
+
+        Args:
+            fund_round is used as for loop attribute.
+
+        Returns:
+            Function return rounds data
+        """
+        return Rounds(
+            fund_id=fund_round.get("fund_id"),
+            round_title=fund_round.get("round_title"),
+            round_id=fund_round.get("round_id"),
+            eligibility_criteria=fund_round.get("eligibility_criteria"),
+            opens=fund_round.get("opens"),
+            deadline=fund_round.get("deadline"),
+            application_url=fund_round.get("application_url"),
+        )
 
     @property
     def opens_formatted(self):
         """
-        GIVEN function is to alter the datetime string object 
+        GIVEN function is to alter the datetime string object
         for self.opens for human readability.
-        """      
-        return datetime.strptime(self.opens, '%Y-%m-%dT%H:%M:%S').strftime('%Y-%m-%d')
-    
+        """
+        return datetime.strptime(self.opens, "%Y-%m-%dT%H:%M:%S").strftime(
+            "%Y-%m-%d"
+        )
+
     @property
     def deadline_formatted(self):
         """
-        GIVEN function is to alter the datetime string object 
+        GIVEN function is to alter the datetime string object
         for self.deadline for human readability.
-        """     
-        return datetime.strptime(self.deadline, '%Y-%m-%dT%H:%M:%S').strftime('%Y-%m-%d')
+        """
+        return datetime.strptime(self.deadline, "%Y-%m-%dT%H:%M:%S").strftime(
+            "%Y-%m-%d"
+        )
 
     @property
     def eligibility_criteria_formatted(self):
-        return "{:,}".format(self.eligibility_criteria['max_project_cost'])
+        """_summary_ Function formats number with comma separated integer
+
+        Returns:
+            Comma separated integers
+        """
+        return "{:,}".format(self.eligibility_criteria["max_project_cost"])
