0b1a5f95ce43ace82dd47e927162ff7f352a3975
Ram <rmn24in@gmail.com>
2022-03-16 09:40:50 +0000

    functions refactored

--- app/discovery/routes.py
+++ app/discovery/routes.py
@@ -1,72 +1,72 @@
-from flask import Blueprint, render_template
-from app.discovery.forms import SearchForm
-from app.discovery.data import query_fund, query_fund_rounds
-from app.discovery.models.rounds import RoundStore
 import requests
-from app.config import (
-    FUND_STORE_API_HOST, ROUND_STORE_API_HOST, FUND_ENDPOINT, ROUND_ENDPOINT
-)
+from app.config import FUND_ENDPOINT
+from app.config import FUND_SEARCH_ENDPOINT
+from app.config import FUND_STORE_API_HOST
+from app.config import ROUND_ENDPOINT
+from app.config import ROUND_STORE_API_HOST
+from app.discovery.data import query_fund
+from app.discovery.forms import SearchForm
+from app.discovery.models.rounds import Rounds
+from flask import Blueprint
+from flask import flash
+from flask import render_template
 
-discovery_bp = Blueprint("discovery_bp", __name__,
-  template_folder="templates")
-
-"""
-query is a global variable which is used 
-in both routes to send the query to fund store database
-"""
-QUERY = []
+discovery_bp = Blueprint("discovery_bp", __name__, template_folder="templates")
 
 
-@discovery_bp.route('/', methods=['GET', 'POST'])
+@discovery_bp.route("/", methods=["GET", "POST"])
 def search_fund():
-    """ 
+    """
     Given function creates a text field for search box &
-    renders on index.html then 
-    retrives input data from the user, run that data through 
-    function query_fund from data.py to grab the fund 
-    & renders back onto index.html 
+    renders on index.html then
+    retrives input data from the user, run that data through
+    function query_fund from data.py to grab the fund
+    & renders back onto index.html
     """
     form = SearchForm()
     if not form.validate_on_submit():
-        return render_template("search.html", form=form)    
+        return render_template("search.html", form=form)
     else:
-        global QUERY
         QUERY = form.search.data.split(" ")
         fund_results = query_fund(
-            QUERY, f"{FUND_STORE_API_HOST}/{FUND_ENDPOINT}")
-        return render_template("search.html", query =QUERY, 
-                              fund_results = fund_results,
-                              form=form)                          
-    
+            QUERY,
+            f"{FUND_STORE_API_HOST}/{FUND_ENDPOINT}/{FUND_SEARCH_ENDPOINT}",
+        )
+        return render_template(
+            "search.html", query=QUERY, fund_results=fund_results, form=form
+        )
 
-@discovery_bp.route('/round/<id>', methods=['GET', 'POST'])
-def fund_rounds(id):
+
+@discovery_bp.route("/round/<fund_id>", methods=["GET", "POST"])
+def funds(fund_id):
     """
-    GIVEN Function calls the RoundStore function 
+    GIVEN Function calls the RoundStore function
     from data model to check rounds with given endpoint
-     & id. 
+     & id.
      Function query_fund send QUERY to fund store
      so the fund name can be displayed onto the rounds page.
     """
-    response  = requests.get(
-        f"{ROUND_STORE_API_HOST}/{ROUND_ENDPOINT}/{id}")
+    response = requests.get(
+        f"{ROUND_STORE_API_HOST}/{ROUND_ENDPOINT}/{fund_id}"
+    )
     if response.status_code == 200:
         fund_rounds_data = response.json()
         fund_details = []
         if fund_rounds_data:
             for fund_round in fund_rounds_data:
-                rounds_data = query_fund_rounds(fund_round)
+                rounds_data = Rounds.fund_rounds(fund_round)
                 fund_details.append(rounds_data)
     else:
-        message = "No rounds exist for this fund"
-        return render_template("fund.html", 
-                        message = message
-                        )
-    fund_results = query_fund(
-        QUERY, f"{FUND_STORE_API_HOST}/{FUND_ENDPOINT}")
+        flash("No rounds exist for this fund")
+        error = "No rounds exist for this fund"
+        return render_template("fund.html", error=error)
 
-    return render_template("fund.html", 
-                            fund_details = fund_details, 
-                            fund_results = fund_results    
-                        )
+    response = requests.get(f"{FUND_STORE_API_HOST}/{FUND_ENDPOINT}/{fund_id}")
+    if response.status_code == 200:
+        fund_json_response = response.json()
 
+    return render_template(
+        "fund.html",
+        fund_details=fund_details,
+        fund_json_response=fund_json_response,
+    )
