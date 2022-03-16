892dccdc0b58fc2783dd7ce32c35b6ccfc465e30
Ram <rmn24in@gmail.com>
2022-03-16 09:38:23 +0000

    flake8 compliance

--- tests/test_data_response.py
+++ tests/test_data_response.py
@@ -1,24 +1,25 @@
 from urllib.request import urlopen
-from flask import url_for
+
 import pytest
 from app.create_app import create_app
+from flask import url_for
+
 
 @pytest.fixture(scope="session")
 def app():
     app = create_app()
     return app
 
-@pytest.mark.usefixtures('live_server')
-class TestLiveServer:
+
+@pytest.mark.usefixtures("live_server")
+class TestSearchPage:
     """
-    GIVEN class checks if routes are up & running
+    GIVEN class checks if route for search_fund
+    is up & running
     """
 
     def test_search_page_response(self):
-        url = url_for('discovery_bp.search_fund', _external=True)
+        url = url_for("discovery_bp.search_fund", _external=True)
         res = urlopen(url)
-        assert b'search' in res.read()
+        assert b"search" in res.read()
         assert res.code == 200
-
-
-
