be80d1935a9c5860c1d08b2f27e044fa84800ff4
Ram <rmn24in@gmail.com>
2022-03-16 09:43:50 +0000

    unused imports removed

--- tests/test_accessibility.py
+++ tests/test_accessibility.py
@@ -1,20 +1,23 @@
-from selenium import webdriver
-from axe_selenium_python import Axe
-from chromedriver_py import binary_path
-from selenium.webdriver.chrome.options import Options
+import pytest
 from app.config import get_endpoints
+from axe_selenium_python import Axe
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import pytest
+
 
 @pytest.fixture(scope="class")
 def selenium_chrome_driver():
     global driver
     options = Options()
     options.add_argument("--headless")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    yield 
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()), options=options
+    )
+    yield
     driver.close()
+
 
 @pytest.mark.usefixtures("selenium_chrome_driver")
 @pytest.mark.accessibility
@@ -27,18 +30,10 @@ def test_run_axe():
         # Run axe accessibility checks.
         results = axe.run()
         # Write results to file
-        axe.write_results(results, 'tests/accessibility_report.json')
+        axe.write_results(results, "tests/accessibility_report.json")
         # Assert no of violations are found
         assert len(results["violations"]) <= 1
         assert (
             len(results["violations"]) == 0
             or results["violations"][0]["impact"] == "minor"
         ), axe.report(results["violations"])
-
-
-
-
-
-
-
-
