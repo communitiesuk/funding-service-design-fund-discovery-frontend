2ca284150559d61dc667e02b15d5d4c27d36e818
Ram <rmn24in@gmail.com>
2022-05-14 12:00:00 +0100

    config file- add env for authentocator api host

--- app/config.py
+++ app/config.py
@@ -20,6 +20,7 @@ host api doesn't not work/respond
 TEST_FUND_STORE_API_HOST = "fund_store"
 TEST_ROUND_STORE_API_HOST = "round_store"
 TEST_ACCOUNT_STORE_API_HOST = "http://localhost:6794"
+TEST_AUTHENTICATOR_API_HOST = "http://localhost:6700"
 
 
 FUND_STORE_API_HOST = (
@@ -35,6 +36,11 @@ ACCOUNT_STORE_API_HOST = (
     os.environ.get("ACCOUNT_STORE_API_HOST") or TEST_ACCOUNT_STORE_API_HOST
 )
 
+AUTHENTICATOR_STORE_API_HOST = (
+    os.environ.get("AUTHENTICATOR_STORE_API_HOST")
+    or os.environ.get("TEST_AUTHENTICATOR_STORE_API_HOST")
+    or TEST_AUTHENTICATOR_API_HOST
+)
 
 FUNDS_URL = "{host}/funds/{fund_id}"
 FUNDS_SEARCH_URL = "{host}/funds/search/?"
