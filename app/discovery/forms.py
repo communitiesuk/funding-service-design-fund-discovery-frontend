6b718879303e60f93fe32273e3f4f4cb5f49bad8
Ram <rmn24in@gmail.com>
2022-05-16 10:02:47 +0100

    user email form removed - moved to authenticator

--- app/discovery/forms.py
+++ app/discovery/forms.py
@@ -1,8 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import EmailField
 from wtforms import StringField
 from wtforms.validators import DataRequired
-from wtforms.validators import Email
 
 
 class SearchForm(FlaskForm):
@@ -11,13 +9,3 @@ class SearchForm(FlaskForm):
     """
 
     search = StringField("Search fund", validators=[DataRequired()])
-
-
-class EmailForm(FlaskForm):
-    """EmailForm This class contains the logic for the email form.
-
-    A FlaskForm combined with some logic for handling the email address
-    submission.
-    """
-
-    email = EmailField("email field", validators=[Email()])
