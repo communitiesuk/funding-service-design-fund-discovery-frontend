c46f1ce1e76938ea53bd2b90f4c5bc0134bda826
Ram <rmn24in@gmail.com>
2022-05-19 10:50:27 +0100

    ⚡️  Routes refactored with to grab all funds if no search keyword provided.

--- app/discovery/routes.py
+++ app/discovery/routes.py
@@ -5,51 +5,37 @@ from app.config import FUNDS_URL
 from app.config import ROUND_STORE_API_HOST
 from app.config import ROUNDS_URL
 from app.discovery.forms import SearchForm
-from app.discovery.models.data import convert_none_to_string
 from app.discovery.models.data import get_fund_name
 from app.discovery.models.data import list_data
 from app.discovery.models.data import query_fund
 from app.discovery.models.data import query_rounds
 from app.discovery.models.rounds import Rounds
 from flask import Blueprint
-from flask import redirect
 from flask import render_template
 from flask import request
-from flask import url_for
+
 
 discovery_bp = Blueprint("discovery_bp", __name__, template_folder="templates")
 
 
-@discovery_bp.route("/", methods=["GET", "POST"])
+@discovery_bp.route("/")
 def search_funds():
     """GIVEN route takes a search form, retrieves
     user input/query & return query response
     from fund store.
     """
     form = SearchForm()
-    query = request.args.get("query_fund")
+    query = request.args.get("search", "")
 
-    if query is not None:
-        query_response = query_fund(
-            query, FUNDS_SEARCH_URL.format(host=FUND_STORE_API_HOST)
-        )
-        return render_template(
-            "search.html",
-            query=query,
-            query_response=query_response,
-            form=form,
-        )
-
-    if not form.validate_on_submit():
-        return render_template(
-            "search.html",
-            form=form,
-        )
-    else:
-        form_data = convert_none_to_string(form.search.data)
-        return redirect(
-            url_for("discovery_bp.search_funds") + "/?query_fund=" + form_data
-        )
+    query_response = query_fund(
+        query, FUNDS_SEARCH_URL.format(host=FUND_STORE_API_HOST)
+    )
+    return render_template(
+        "search.html",
+        query=query,
+        query_response=query_response,
+        form=form,
+    )
 
 
 @discovery_bp.route("/round/<fund_id>", methods=["GET", "POST"])
