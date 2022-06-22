3feaeaa299964e922c72e5f289dfd1f5310da3aa
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-06-22 15:14:52 +0100

    :art: Added test scoped imports.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,34 +1,25 @@
 """
 Contains test configuration.
 """
-import os
-from unittest import mock
-
 import pytest
-from app.create_app import create_app
 
 
 @pytest.fixture()
-def flask_test_client(mocker):
+def flask_test_client():
     """
     Creates the test client we will be using to test the responses
     from our app, this is a test fixture.
     :return: A flask test client.
     """
+    from app.create_app import create_app
+
     with create_app().test_client() as test_client:
         yield test_client
-        os.environ["FLASK_DEBUG"] = "0"
-
-
-@pytest.fixture(autouse=True, scope="session")
-def mock_env_vars():
-    with mock.patch.dict(
-        os.environ, {"FLASK_ENV": "unit_test", "FLASK_DEBUG": "1"}
-    ):
-        yield
 
 
 @pytest.fixture(scope="session")
 def app():
+    from app.create_app import create_app
+
     app = create_app()
     yield app
