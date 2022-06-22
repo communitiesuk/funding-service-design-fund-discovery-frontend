082864544ccef23810c01952360c2c9cb10d41c7
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-06-22 14:23:40 +0100

    :bug: Tests now run - debug mode manually set.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
 """
 Contains test configuration.
 """
+import os
+
+import config
 import pytest
 from app.create_app import create_app
+from config.unit_test import UnitTestConfig
 
 
 @pytest.fixture()
@@ -12,11 +16,16 @@ def flask_test_client(mocker):
     from our app, this is a test fixture.
     :return: A flask test client.
     """
+    os.environ["FLASK_DEBUG"] = "1"
+    mocker.patch.object(config, "Config", UnitTestConfig)
     with create_app().test_client() as test_client:
         yield test_client
+        os.environ["FLASK_DEBUG"] = "0"
 
 
 @pytest.fixture(scope="session")
 def app():
+    os.environ["FLASK_DEBUG"] = "1"
     app = create_app()
     yield app
+    os.environ["FLASK_DEBUG"] = "0"
