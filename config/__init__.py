fc4c6deb728d3fc21bf860da5b9f5fcf6aa94905
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-06-22 14:02:46 +0100

    :bug: Fixed test config loading

--- config/__init__.py
+++ config/__init__.py
@@ -7,9 +7,9 @@ match FLASK_ENV:
         from config.dev import DevConfig as Config  # noqa
     case "development":
         from config.development import DevelopmentConfig as Config  # noqa
-    case "unit_test":
-        from config.test import TestConfig as Config  # noqa
     case "test":
+        from config.test import TestConfig as Config  # noqa
+    case "unit_test":
         from config.unit_test import UnitTestConfig as Config  # noqa
     case "production":
         pass
