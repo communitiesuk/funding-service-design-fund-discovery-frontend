2e2076aa3ccec94eecb25bccd661e3abc0840603
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2022-06-27 10:11:56 +0100

    make development config overridable from ENV

--- config/environments/development.py
+++ config/environments/development.py
@@ -1,3 +1,5 @@
+import os
+
 from config.environments.default import DefaultConfig
 from fsd_utils import configclass
 
@@ -5,5 +7,5 @@ from fsd_utils import configclass
 @configclass
 class DevelopmentConfig(DefaultConfig):
 
-    USE_LOCAL_DATA = True
-    FUND_STORE_API_HOST = "fund_store"
+    USE_LOCAL_DATA = os.environ.get("USE_LOCAL_DATA", True)
+    FUND_STORE_API_HOST = os.environ.get("FUND_STORE_API_HOST", "fund_store")
