47586c70ca5de0799c13aa839f21d00cc4c4713a
Adam <adam.wallace@communities.gov.uk>
2022-07-22 12:23:22 +0100

    get from env var first

--- config/envs/development.py
+++ config/envs/development.py
@@ -1,5 +1,5 @@
 from os import environ
-
+import os
 from config.envs.default import DefaultConfig
 from distutils.util import strtobool
 from fsd_utils import configclass
@@ -10,8 +10,8 @@ import logging
 class DevelopmentConfig(DefaultConfig):
 
     SECRET_KEY = "dev_secret_key"
-    FUND_STORE_API_HOST = "fund_store"
-    AUTHENTICATOR_HOST = "https://funding-service-design-authenticator-dev.london.cloudapps.digital"
+    FUND_STORE_API_HOST = os.getenv("FUND_STORE_API_HOST", "fund_store")
+    AUTHENTICATOR_HOST = os.getenv("AUTHENTICATOR_HOST", "https://funding-service-design-authenticator-dev.london.cloudapps.digital")
     USE_LOCAL_DATA = strtobool(environ.get("USE_LOCAL_DATA", "True"))
 
     if AUTHENTICATOR_HOST:
